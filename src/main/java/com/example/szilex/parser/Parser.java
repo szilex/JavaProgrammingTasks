3eb59b42bb8334d1d8a368d6118f48bbd111cfe7
szilex <32484638+szilex@users.noreply.github.com>
2021-10-14 20:06:46 +0200

    Create empty implementation with tests

---
+++ src/main/java/com/example/szilex/parser/Parser.java
@@ -0,0 +1,8 @@
+package com.example.szilex.parser;
+
+public class Parser {
+    public static int parseInt(String numberString) {
+        
+        return -1;
+    }
+}
---
+++ src/test/java/com/example/szilex/parser/ParserTests.java
@@ -0,0 +1,92 @@
+package com.example.szilex.parser;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ParserTests {
+
+    @Test
+    void testParseIntZeroString() {
+        String numberString = "zero";
+        assertEquals(0, Parser.parseInt(numberString));
+    }
+
+    @Test
+    void testParseIntOneString() {
+        String numberString = "one";
+        assertEquals(1, Parser.parseInt(numberString));
+    }
+
+    @Test
+    void testParseIntTenString() {
+        String numberString = "ten";
+        assertEquals(10, Parser.parseInt(numberString));
+    }
+
+    @Test
+    void testParseIntFifteenString() {
+        String numberString = "fifteen";
+        assertEquals(15, Parser.parseInt(numberString));
+    }
+
+    @Test
+    void testParseIntTwentyTwoStringWithoutDash() {
+        String numberString = "twenty two";
+        assertEquals(22, Parser.parseInt(numberString));
+    }
+
+    @Test
+    void testParseIntThirtySevenStringWithDash() {
+        String numberString = "thirty-seven";
+        assertEquals(37, Parser.parseInt(numberString));
+    }
+
+    @Test
+    void testParseIntSeventyNineStringWithDashAndCapitalLetters() {
+        String numberString = "Seventy-nine";
+        assertEquals(79, Parser.parseInt(numberString));
+    }
+
+    @Test
+    void testParseIntOneHundredString() {
+        String numberString = "one hundred";
+        assertEquals(100, Parser.parseInt(numberString));
+    }
+
+    @Test
+    void testParseIntOneHundredTwentyThreeString() {
+        String numberString = "one hundred twenty three";
+        assertEquals(123, Parser.parseInt(numberString));
+    }
+
+    @Test
+    void testParseIntOneHundredAndSixtyOneString() {
+        String numberString = "one hundred and sixty one";
+        assertEquals(161, Parser.parseInt(numberString));
+    }
+
+    @Test
+    void testParseIntFourThousandSixHundredFiftyEightStringWithDash() {
+        String numberString = "four thousand six hundred fifty-eight";
+        assertEquals(4658, Parser.parseInt(numberString));
+    }
+
+    @Test
+    void testParseIntTwentyFourThousandFortyTwoString() {
+        String numberString = "twenty four thousand forty two";
+        assertEquals(42042, Parser.parseInt(numberString));
+    }
+
+    @Test
+    void testParseIntSevenHundredEightyThreeThousandNineHundredAndNineteenString() {
+        String numberString = "seven hundred eighty-three thousand nine hundred and nineteen";
+        assertEquals(783919, Parser.parseInt(numberString));
+    }
+
+    @Test
+    void testParseIntOneMillionString() {
+        String numberString = "four thousand six hundred fifty-eight";
+        assertEquals(100, Parser.parseInt(numberString));
+    }
+}
