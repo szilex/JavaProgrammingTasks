6657aa4efa4aeba4689f2f7ad0897da5381d1bfd
szilex <32484638+szilex@users.noreply.github.com>
2021-10-14 21:22:45 +0200

    Provide simple implementation for numbers 0-1000000

--- src/main/java/com/example/szilex/parser/NumberString.java
+++ src/main/java/com/example/szilex/parser/NumberString.java
@@ -1,5 +1,10 @@
 package com.example.szilex.parser;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ListIterator;
+
 public enum NumberString {
     ZERO(0),
     ONE(1),
@@ -37,5 +42,58 @@ public enum NumberString {
 
     NumberString(int value) {
         this.value = value;
+    }
+
+    public static int calculateValue(String[] valueStringArray) {
+        int result = 0;
+        List<String> formattedNumberList = new ArrayList<>(Arrays.asList(valueStringArray));
+        ListIterator<String> listIterator = formattedNumberList.listIterator();
+        while(listIterator.hasNext()) {
+            int partialSum=0;
+            loop: while(listIterator.hasNext()) {
+                String value = listIterator.next().toUpperCase();
+                switch (NumberString.valueOf(value)) {
+                    case HUNDRED: {
+                        if (partialSum == 0) {
+                            partialSum = HUNDRED.value;
+                        } else {
+                            partialSum *= HUNDRED.value;
+                        }
+                        int offset = 0;
+                        while(listIterator.hasNext()) {
+                            offset++;
+                            String tempValue = listIterator.next();
+                            if (NumberString.valueOf(tempValue.toUpperCase()).equals(THOUSAND)) {
+                                partialSum *= THOUSAND.value;
+                            }
+                        }
+                        for (; offset>0; offset--) {
+                            listIterator.previous();
+                        }
+                        break loop;
+                    }
+                    case THOUSAND: {
+                        if (partialSum == 0) {
+                            partialSum = THOUSAND.value;
+                        } else {
+                            partialSum *= THOUSAND.value;
+                        }
+                        break loop;
+                    }
+                    case MILLION: {
+                        if (partialSum == 0) {
+                            partialSum = MILLION.value;
+                        } else {
+                            partialSum *= MILLION.value;
+                        }
+                        break loop;
+                    }
+                    default:
+                        partialSum += NumberString.valueOf(value).value;
+                }
+            }
+            result += partialSum;
+        }
+        return result;
     }
 }
--- src/main/java/com/example/szilex/parser/Parser.java
+++ src/main/java/com/example/szilex/parser/Parser.java
@@ -4,19 +4,8 @@ public class Parser {
     public static int parseInt(String numberString) {
 
         StringInputFormatter formatter = new StringInputFormatter();
-        String[] formattedNumberString = formatter.formatString(numberString);
+        String[] formattedNumberArray = formatter.formatString(numberString);
 
-        int result = 0;
-
-        for (String value : formattedNumberString) {
-            System.out.println(value.toUpperCase());
-            result += NumberString.valueOf(value.toUpperCase()).value;
-        }
-
-        return result;
-    }
-
-    public static void main(String[] args) {
-        parseInt("one");
+        return NumberString.calculateValue(formattedNumberArray);
     }
 }
