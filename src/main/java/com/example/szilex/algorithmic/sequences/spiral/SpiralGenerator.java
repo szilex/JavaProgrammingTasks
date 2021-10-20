642a4c3812623f1af972c6a1113b6bdbaefd5c9b
szilex <32484638+szilex@users.noreply.github.com>
2021-10-20 20:21:54 +0200

    Provide implementation for getSpiral function

--- src/main/java/com/example/szilex/algorithmic/sequences/spiral/SpiralGenerator.java
+++ src/main/java/com/example/szilex/algorithmic/sequences/spiral/SpiralGenerator.java
@@ -2,9 +2,73 @@ package com.example.szilex.algorithmic.sequences.spiral;
 
 public class SpiralGenerator {
 
+    private enum SpiralDirection {
+        RIGHT,
+        DOWN,
+        LEFT,
+        UP
+    }
+
     public static int[][] getSpiral(int n) {
+        if (n < 5) {
+            throw new IllegalArgumentException("Incorrect spiral size: " + n);
+        }
 
+        int[][] result = new int[n][n];
+        int rowIndex = 0, columnIndex = 0;
+        int rowIndexLowerBound = 2, columnIndexLowerBound = 0;
+        int rowIndexUpperBound = n - 1, columnIndexUpperBound = n - 1;
+        int cellAmount = n * n;
+        SpiralDirection currentDirection = SpiralDirection.RIGHT;
 
-        return null;
+        for (int i = 0; i < cellAmount; i++) {
+            System.out.println("Index: " + rowIndex + "," + columnIndex);
+            result[rowIndex][columnIndex] = 1;
+            switch (currentDirection) {
+                case RIGHT:
+                    if (columnIndex == columnIndexUpperBound) {
+                        currentDirection = SpiralDirection.DOWN;
+                        rowIndex++;
+                        columnIndexUpperBound -= 2;
+                    } else {
+                        columnIndex++;
+                    }
+                    break;
+                case DOWN:
+                    if (rowIndex == rowIndexUpperBound) {
+                        currentDirection = SpiralDirection.LEFT;
+                        columnIndex--;
+                        rowIndexUpperBound -= 2;
+                    } else {
+                        rowIndex++;
+                    }
+                    break;
+                case LEFT:
+                    if (columnIndex == columnIndexLowerBound) {
+                        currentDirection = SpiralDirection.UP;
+                        rowIndex--;
+                        columnIndexLowerBound += 2;
+                    } else {
+                        columnIndex--;
+                    }
+                    break;
+                case UP:
+                    if (rowIndex == rowIndexLowerBound) {
+                        currentDirection = SpiralDirection.RIGHT;
+                        columnIndex++;
+                        rowIndexLowerBound += 2;
+                    } else {
+                        rowIndex--;
+                    }
+                    break;
+            }
+            if (columnIndexLowerBound > columnIndexUpperBound && rowIndexLowerBound > rowIndexUpperBound) {
+                if (n % 2 == 0) {
+                    result[rowIndex][columnIndex] = 1;
+                }
+                break;
+            }
+        }
+        return result;
     }
 }
