c3f52be888aa3251022dd1721b711df5c546683d
szilex <32484638+szilex@users.noreply.github.com>
2021-10-22 22:43:06 +0200

    Add missing Test annotations

--- src/test/java/com/example/szilex/algorithmic/matrices/MatrixUtilsTests.java
+++ src/test/java/com/example/szilex/algorithmic/matrices/MatrixUtilsTests.java
@@ -17,21 +17,25 @@ public class MatrixUtilsTests {
         Assertions.assertEquals(-2, MatrixUtils.calculateDeterminant(matrix));
     }
 
+    @Test
     void testThreeByThreeElementMatrix() {
         int[][] matrix = new int[][] {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         Assertions.assertEquals(0, MatrixUtils.calculateDeterminant(matrix));
     }
 
+    @Test
     void testThreeByThreeNegativeElementMatrix() {
         int[][] matrix = new int[][] {{1, -2, -3}, {-4, -5, 6}, {7, -8, 9}};
         Assertions.assertEquals(-354, MatrixUtils.calculateDeterminant(matrix));
     }
 
+    @Test
     void testThreeByThreeDiagonalMatrix() {
         int[][] matrix = new int[][] {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
         Assertions.assertEquals(1, MatrixUtils.calculateDeterminant(matrix));
     }
 
+    @Test
     void testFiveByFiveElementMatrix() {
         int[][] matrix = new int[][] {{1, 2, 3, 4, 5}, {7, 9, 3, 2, 3}, {6, 2, 8, 4, 7}, {3, 1, 8, 4, 5}, {6, 3, 5, 3, 1}};
         Assertions.assertEquals(-1959, MatrixUtils.calculateDeterminant(matrix));
