5027a06585bb4cc271b7a6381a8e29d07435df42
szilex <32484638+szilex@users.noreply.github.com>
2021-10-22 22:41:06 +0200

    Create calculateDeterminant function and tests

---
+++ src/main/java/com/example/szilex/algorithmic/matrices/MatrixUtils.java
@@ -0,0 +1,9 @@
+package com.example.szilex.algorithmic.matrices;
+
+public class MatrixUtils {
+    public static int calculateDeterminant(int[][] matrix) {
+
+
+        return 0;
+    }
+}
---
+++ src/test/java/com/example/szilex/algorithmic/matrices/MatrixUtilsTests.java
@@ -0,0 +1,39 @@
+package com.example.szilex.algorithmic.matrices;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MatrixUtilsTests {
+
+    @Test
+    void testSingleElementMatrix() {
+        int[][] matrix = new int[][] {{1}};
+        Assertions.assertEquals(1, MatrixUtils.calculateDeterminant(matrix));
+    }
+
+    @Test
+    void testTwoByTwoElementMatrix() {
+        int[][] matrix = new int[][] {{1, 2}, {3, 4}};
+        Assertions.assertEquals(-2, MatrixUtils.calculateDeterminant(matrix));
+    }
+
+    void testThreeByThreeElementMatrix() {
+        int[][] matrix = new int[][] {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        Assertions.assertEquals(0, MatrixUtils.calculateDeterminant(matrix));
+    }
+
+    void testThreeByThreeNegativeElementMatrix() {
+        int[][] matrix = new int[][] {{1, -2, -3}, {-4, -5, 6}, {7, -8, 9}};
+        Assertions.assertEquals(-354, MatrixUtils.calculateDeterminant(matrix));
+    }
+
+    void testThreeByThreeDiagonalMatrix() {
+        int[][] matrix = new int[][] {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+        Assertions.assertEquals(1, MatrixUtils.calculateDeterminant(matrix));
+    }
+
+    void testFiveByFiveElementMatrix() {
+        int[][] matrix = new int[][] {{1, 2, 3, 4, 5}, {7, 9, 3, 2, 3}, {6, 2, 8, 4, 7}, {3, 1, 8, 4, 5}, {6, 3, 5, 3, 1}};
+        Assertions.assertEquals(-1959, MatrixUtils.calculateDeterminant(matrix));
+    }
+}
