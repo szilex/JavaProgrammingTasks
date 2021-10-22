2bc11dc49ad9a8aaa3f7437d3946e161de0e0db8
szilex <32484638+szilex@users.noreply.github.com>
2021-10-22 22:50:10 +0200

    Implement determinant calculation for 1x1 and 2x2 matrices

--- src/main/java/com/example/szilex/algorithmic/matrices/MatrixUtils.java
+++ src/main/java/com/example/szilex/algorithmic/matrices/MatrixUtils.java
@@ -1,8 +1,16 @@
 package com.example.szilex.algorithmic.matrices;
 
 public class MatrixUtils {
-    public static int calculateDeterminant(int[][] matrix) {
-
+    public static int calculateSquareMatrixDeterminant(int[][] matrix) {
+        if (matrix.length == 1) {
+            return matrix[0][0];
+        } else if (matrix.length == 2) {
+            return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+        } else {
+            for (int i = 0; i < matrix.length; i++) {
+                //TODO Add logic for extracting sub-matrices and calculate their determinant
+            }
+        }
 
         return 0;
     }
--- src/test/java/com/example/szilex/algorithmic/matrices/MatrixUtilsTests.java
+++ src/test/java/com/example/szilex/algorithmic/matrices/MatrixUtilsTests.java
@@ -8,36 +8,36 @@ public class MatrixUtilsTests {
     @Test
     void testSingleElementMatrix() {
         int[][] matrix = new int[][] {{1}};
-        Assertions.assertEquals(1, MatrixUtils.calculateDeterminant(matrix));
+        Assertions.assertEquals(1, MatrixUtils.calculateSquareMatrixDeterminant(matrix));
     }
 
     @Test
     void testTwoByTwoElementMatrix() {
         int[][] matrix = new int[][] {{1, 2}, {3, 4}};
-        Assertions.assertEquals(-2, MatrixUtils.calculateDeterminant(matrix));
+        Assertions.assertEquals(-2, MatrixUtils.calculateSquareMatrixDeterminant(matrix));
     }
 
     @Test
     void testThreeByThreeElementMatrix() {
         int[][] matrix = new int[][] {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        Assertions.assertEquals(0, MatrixUtils.calculateDeterminant(matrix));
+        Assertions.assertEquals(0, MatrixUtils.calculateSquareMatrixDeterminant(matrix));
     }
 
     @Test
     void testThreeByThreeNegativeElementMatrix() {
         int[][] matrix = new int[][] {{1, -2, -3}, {-4, -5, 6}, {7, -8, 9}};
-        Assertions.assertEquals(-354, MatrixUtils.calculateDeterminant(matrix));
+        Assertions.assertEquals(-354, MatrixUtils.calculateSquareMatrixDeterminant(matrix));
     }
 
     @Test
     void testThreeByThreeDiagonalMatrix() {
         int[][] matrix = new int[][] {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
-        Assertions.assertEquals(1, MatrixUtils.calculateDeterminant(matrix));
+        Assertions.assertEquals(1, MatrixUtils.calculateSquareMatrixDeterminant(matrix));
     }
 
     @Test
     void testFiveByFiveElementMatrix() {
         int[][] matrix = new int[][] {{1, 2, 3, 4, 5}, {7, 9, 3, 2, 3}, {6, 2, 8, 4, 7}, {3, 1, 8, 4, 5}, {6, 3, 5, 3, 1}};
-        Assertions.assertEquals(-1959, MatrixUtils.calculateDeterminant(matrix));
+        Assertions.assertEquals(-1959, MatrixUtils.calculateSquareMatrixDeterminant(matrix));
     }
 }
