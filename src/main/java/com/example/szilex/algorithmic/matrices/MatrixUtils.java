271cab6823a046f307f635cdaefc41c0087951b6
szilex <32484638+szilex@users.noreply.github.com>
2021-10-27 19:55:40 +0200

    Implement determinant calculation using arrays and recursion

--- src/main/java/com/example/szilex/algorithmic/matrices/MatrixUtils.java
+++ src/main/java/com/example/szilex/algorithmic/matrices/MatrixUtils.java
@@ -7,11 +7,21 @@ public class MatrixUtils {
         } else if (matrix.length == 2) {
             return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
         } else {
+            int result = 0;
             for (int i = 0; i < matrix.length; i++) {
-                //TODO Add logic for extracting sub-matrices and calculate their determinant
+                int[][] subArray = new int[matrix.length - 1][matrix.length - 1];
+                for (int matrixRowIndex = 1, subArrayRowIndex=0; matrixRowIndex < matrix.length; matrixRowIndex++, subArrayRowIndex++) {
+                    for (int matrixColumnIndex = 0, subArrayColumnIndex = 0; matrixColumnIndex < matrix.length; matrixColumnIndex++, subArrayColumnIndex++) {
+                        if (matrixColumnIndex != i) {
+                            subArray[subArrayRowIndex][subArrayColumnIndex] = matrix[matrixRowIndex][matrixColumnIndex];
+                        } else {
+                            subArrayColumnIndex--;
+                        }
+                    }
+                }
+                result += (i % 2 == 0 ? 1 : -1) * matrix[0][i] * calculateSquareMatrixDeterminant(subArray);
             }
+            return result;
         }
-
-        return 0;
     }
 }
