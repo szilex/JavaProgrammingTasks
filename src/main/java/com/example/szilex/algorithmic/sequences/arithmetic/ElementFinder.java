91d485e8a200e08eeed99a6b73707441bdb78a24
szilex <32484638+szilex@users.noreply.github.com>
2021-11-03 20:30:41 +0100

    Create findElementInCombinedSequence function and tests

---
+++ src/main/java/com/example/szilex/algorithmic/sequences/arithmetic/ElementFinder.java
@@ -0,0 +1,18 @@
+package com.example.szilex.algorithmic.sequences.arithmetic;
+
+/**
+ * Class aggregating various functions for calculating elements in a given arithmetic sequence
+ */
+public class ElementFinder {
+
+    /**
+     * Function for calculating element on n-th position (index) in an infinite sequence 112123123412345... created from sequence [1, 12, 123, 1234, 12345, ...]
+     * Note: After number 9 is added at the end, the next added value is 10, when 99 is added, then next element is 100 etc.
+     * @param n Index of the desired element, 0 <= n <= 10^18
+     * @return Element at given index
+     */
+    public static int findElementInCombinedSequence(long n) {
+
+        return 0;
+    }
+}
---
+++ src/test/java/com/example/szilex/algorithmic/sequences/arithmetic/ElementFinderTests.java
@@ -0,0 +1,67 @@
+package com.example.szilex.algorithmic.sequences.arithmetic;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ElementFinderTests {
+
+    @Test
+    public void testFindElementInCombinedSequenceIndexOne() {
+        Assertions.assertEquals(1L, ElementFinder.findElementInCombinedSequence(1L));
+    }
+
+    @Test
+    public void testFindElementInCombinedSequenceIndexTwo() {
+        Assertions.assertEquals(1L, ElementFinder.findElementInCombinedSequence(2L));
+    }
+
+    @Test
+    public void testFindElementInCombinedSequenceIndexThree() {
+        Assertions.assertEquals(2L, ElementFinder.findElementInCombinedSequence(3L));
+    }
+
+    @Test
+    public void testFindElementInCombinedSequenceIndexSix() {
+        Assertions.assertEquals(3L, ElementFinder.findElementInCombinedSequence(6L));
+    }
+
+    @Test
+    public void testFindElementInCombinedSequenceIndexTen() {
+        Assertions.assertEquals(4L, ElementFinder.findElementInCombinedSequence(10L));
+    }
+
+    @Test
+    public void testFindElementInCombinedSequenceIndexEleven() {
+        Assertions.assertEquals(1L, ElementFinder.findElementInCombinedSequence(11L));
+    }
+
+    @Test
+    public void testFindElementInCombinedSequenceIndexFifteen() {
+        Assertions.assertEquals(5L, ElementFinder.findElementInCombinedSequence(15L));
+    }
+
+    @Test
+    public void testFindElementInCombinedSequenceIndexOneHundred() {
+        Assertions.assertEquals(1L, ElementFinder.findElementInCombinedSequence(100L));
+    }
+
+    @Test
+    public void testFindElementInCombinedSequenceIndexOneHundredTwentyThreeThousandFourHundredAndFiftySix() {
+        Assertions.assertEquals(6L, ElementFinder.findElementInCombinedSequence(123456L));
+    }
+
+    @Test
+    public void testFindElementInCombinedSequenceIndexOneHundredTwentyThreeMillionFourHundredFiftySixThousandSevenHundredAndEightyNine() {
+        Assertions.assertEquals(3L, ElementFinder.findElementInCombinedSequence(123456789L));
+    }
+
+    @Test
+    public void testFindElementInCombinedSequenceIndexTenToPowerEighteenMinusOne() {
+        Assertions.assertEquals(4L, ElementFinder.findElementInCombinedSequence(999999999999999999L));
+    }
+
+    @Test
+    public void testFindElementInCombinedSequenceIndexTenToPowerEighteen() {
+        Assertions.assertEquals(1L, ElementFinder.findElementInCombinedSequence(1000000000000000000L));
+    }
+}
